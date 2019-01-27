bd5634cb88cbb0d4a8614de8ccd5c0e140918188
Hardouin-Brochard <rakoon@outlook.fr>
2019-01-27 22:56:40 +0100

    correction du bug concernant le script balise script a placé apres le chargement des éléments concerner :p

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const userScore_span = document.getElementById('user-score');
 const computerScore_span = document.getElementById('computer-score');
 const scoreBoard_div = document.querySelector(".score-board");
 const result_div = document.querySelector(".result");
-const couche_div = document.getElementById("couche");
+var couche_div = document.getElementById("couche");
 const cub_div = document.getElementById("cub");
 const culotte_div = document.getElementById("culotte");
+
 
 couche_div.addEventListener("click", function() {
 	console.log("tu as cliqué sur couche");
